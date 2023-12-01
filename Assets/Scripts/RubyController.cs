c0d7163ecc5c986cd927365cfa760c95605b369b
javell-f <ja233222@ucf.edu>
2023-11-30 22:31:38 -0500

    mods for ruby

--- Assets/Scripts/RubyController.cs
+++ Assets/Scripts/RubyController.cs
@@ -1,6 +1,6 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class RubyController : MonoBehaviour
 {
@@ -12,6 +12,9 @@ public class RubyController : MonoBehaviour
 
     public AudioClip throwSound;
     public AudioClip hitSound;
+
+    public ParticleSystem gainHealthParticles;
+    public ParticleSystem loseHealthParticles;
 
     public int health { get { return currentHealth; } }
     int currentHealth;
@@ -25,60 +28,73 @@ public class RubyController : MonoBehaviour
     float vertical;
 
     Animator animator;
-    Vector2 lookDirection = new Vector2(1, 0);
 
+    Vector2 lookDirection = new Vector2(1, 0);
     AudioSource audioSource;
 
-    // Start is called before the first frame update
+    public Text gameOverText; 
+
+    bool isGameOver = false; 
+
     void Start()
     {
         rigidbody2d = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
-
         currentHealth = maxHealth;
-
         audioSource = GetComponent<AudioSource>();
+
+        
+        gainHealthParticles = Instantiate(gainHealthParticles);
+        loseHealthParticles = Instantiate(loseHealthParticles);
+
+        
+        if (gameOverText != null)
+        {
+            gameOverText.gameObject.SetActive(false);
+        }
     }
 
-    // Update is called once per frame
     void Update()
     {
-        horizontal = Input.GetAxis("Horizontal");
-        vertical = Input.GetAxis("Vertical");
-
-        Vector2 move = new Vector2(horizontal, vertical);
-
-        if (!Mathf.Approximately(move.x, 0.0f) || !Mathf.Approximately(move.y, 0.0f))
+        if (!isGameOver) 
         {
-            lookDirection.Set(move.x, move.y);
-            lookDirection.Normalize();
-        }
+            horizontal = Input.GetAxis("Horizontal");
+            vertical = Input.GetAxis("Vertical");
 
-        animator.SetFloat("Look X", lookDirection.x);
-        animator.SetFloat("Look Y", lookDirection.y);
-        animator.SetFloat("Speed", move.magnitude);
+            Vector2 move = new Vector2(horizontal, vertical);
 
-        if (isInvincible)
-        {
-            invincibleTimer -= Time.deltaTime;
-            if (invincibleTimer < 0)
-                isInvincible = false;
-        }
-
-        if (Input.GetKeyDown(KeyCode.C))
-        {
-            Launch();
-        }
-
-        if (Input.GetKeyDown(KeyCode.X))
-        {
-            RaycastHit2D hit = Physics2D.Raycast(rigidbody2d.position + Vector2.up * 0.2f, lookDirection, 1.5f, LayerMask.GetMask("NPC"));
-            if (hit.collider != null)
+            if (!Mathf.Approximately(move.x, 0.0f) || !Mathf.Approximately(move.y, 0.0f))
             {
-                NonPlayerCharacter character = hit.collider.GetComponent<NonPlayerCharacter>();
-                if (character != null)
+                lookDirection.Set(move.x, move.y);
+                lookDirection.Normalize();
+            }
+
+            animator.SetFloat("Look X", lookDirection.x);
+            animator.SetFloat("Look Y", lookDirection.y);
+            animator.SetFloat("Speed", move.magnitude);
+
+            if (isInvincible)
+            {
+                invincibleTimer -= Time.deltaTime;
+                if (invincibleTimer < 0)
+                    isInvincible = false;
+            }
+
+            if (Input.GetKeyDown(KeyCode.C))
+            {
+                Launch();
+            }
+
+            if (Input.GetKeyDown(KeyCode.X))
+            {
+                RaycastHit2D hit = Physics2D.Raycast(rigidbody2d.position + Vector2.up * 0.2f, lookDirection, 1.5f, LayerMask.GetMask("NPC"));
+                if (hit.collider != null)
                 {
-                    character.DisplayDialog();
+                    NonPlayerCharacter character = hit.collider.GetComponent<NonPlayerCharacter>();
+                    if (character != null)
+                    {
+                        character.DisplayDialog();
+                    }
                 }
             }
         }
@@ -86,11 +102,14 @@ public class RubyController : MonoBehaviour
 
     void FixedUpdate()
     {
-        Vector2 position = rigidbody2d.position;
-        position.x = position.x + speed * horizontal * Time.deltaTime;
-        position.y = position.y + speed * vertical * Time.deltaTime;
+        if (!isGameOver) 
+        {
+            Vector2 position = rigidbody2d.position;
+            position.x = position.x + speed * horizontal * Time.deltaTime;
+            position.y = position.y + speed * vertical * Time.deltaTime;
 
-        rigidbody2d.MovePosition(position);
+            rigidbody2d.MovePosition(position);
+        }
     }
 
     public void ChangeHealth(int amount)
@@ -103,23 +122,64 @@ public class RubyController : MonoBehaviour
             isInvincible = true;
             invincibleTimer = timeInvincible;
 
+            
+            if (loseHealthParticles != null)
+            {
+                loseHealthParticles.Play();
+            }
+
             PlaySound(hitSound);
+
+            if (currentHealth <= 0)
+            {
+                StartCoroutine(GameOverSequence());
+            }
+        }
+        else if (amount > 0)
+        {
+            
+            if (gainHealthParticles != null)
+            {
+                gainHealthParticles.Play();
+            }
         }
 
         currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
 
+        // Assuming UIHealthBar is a class or component handling the health bar UI
         UIHealthBar.instance.SetValue(currentHealth / (float)maxHealth);
+    }
+
+    IEnumerator GameOverSequence()
+    {
+        isGameOver = true; 
+
+        // Enable the game over text
+        if (gameOverText != null)
+        {
+            gameOverText.gameObject.SetActive(true);
+        }
+
+        while (!Input.GetKeyDown(KeyCode.R))
+        {
+            yield return null;
+        }
+
+        // Restart the game
+        RestartGame();
+    }
+
+    void RestartGame()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(UnityEngine.SceneManagement.SceneManager.GetActiveScene().name);
     }
 
     void Launch()
     {
         GameObject projectileObject = Instantiate(projectilePrefab, rigidbody2d.position + Vector2.up * 0.5f, Quaternion.identity);
-
         Projectile projectile = projectileObject.GetComponent<Projectile>();
         projectile.Launch(lookDirection, 30000);
-
         animator.SetTrigger("Launch");
-
         PlaySound(throwSound);
     }
 
